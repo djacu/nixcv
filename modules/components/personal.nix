8d8d02c54164f8f50be5682f44ac405271e1ee02
Daniel Baker <daniel.n.baker@gmail.com>
2023-12-25 13:52:44 -0800

    Added a personal module. Needs more work on profiles.

---
+++ modules/components/personal.nix
@@ -0,0 +1,110 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) types;
+  cfg = config;
+  #listInfo = {
+  #  optionName = "profiles";
+  #  description = "A list of social media profiles.";
+  #  subType = [../components/socials.nix];
+  #};
+in {
+  options = {
+    name = lib.mkOption {
+      description = "Your full name.";
+      type = types.nullOr types.str;
+      default = null;
+      example = "Johnathan Doe";
+    };
+    label = lib.mkOption {
+      description = "Your job title or general field of expertise.";
+      type = types.nullOr types.str;
+      default = null;
+      example = "Programmer";
+    };
+    email = lib.mkOption {
+      description = "Your email.";
+      type = types.nullOr types.str;
+      default = null;
+      example = "john@doe.com";
+    };
+    phone = lib.mkOption {
+      description = "Your phone number.";
+      type = types.nullOr types.str;
+      default = null;
+      example = "(123) 456-7890";
+    };
+    url = lib.mkOption {
+      description = "Your website.";
+      type = types.nullOr types.str;
+      default = null;
+      example = "https://john.doe.com";
+    };
+    location = lib.mkOption {
+      description = "Your location information.";
+      type = types.nullOr (types.submoduleWith {
+        modules = [
+          ../components/address.nix
+        ];
+      });
+      default = null;
+    };
+    #FIXME: The genericList didn't work as I thought. Need to fix... maybe make it's own module.
+    #profiles = lib.mkOption {
+    #  description = "Your social media profiles.";
+    #  type = types.nullOr (types.submoduleWith {
+    #    modules = [
+    #      (import ../components/genericList.nix listInfo)
+    #    ];
+    #  });
+    #  default = null;
+    #};
+
+    order = lib.mkOption {
+      description = "The order of the profile items.";
+      type = types.listOf types.str;
+      default = [
+        "name"
+        "label"
+        "email"
+        "phone"
+        "url"
+        "location"
+        #"profiles"
+      ];
+    };
+
+    _outPlaintext = lib.mkOption {
+      description = "This modules output.";
+      type = types.str;
+      readOnly = true;
+    };
+  };
+  config = let
+    cfgItems =
+      builtins.removeAttrs
+      cfg
+      ["_module" "_outPlaintext"];
+  in {
+    # FIXME: if it's a str then get the str otherwise it's a submoudle and get _outPlaintext
+    _outPlaintext = (
+      lib.concatStringsSep
+      "\n"
+      ( # get plaintext if its a module otherwise get the value
+        (builtins.map)
+        (x: x._outPlaintext or x)
+        ( # remove the nulls
+          builtins.filter
+          (x: ! builtins.isNull x)
+          ( # extract the options in order
+            builtins.map
+            (x: cfgItems.${x})
+            cfg.order
+          )
+        )
+      )
+    );
+  };
+}
---
+++ modules/test/personal.nix
@@ -0,0 +1,60 @@
+{lib, ...}: let
+  inherit (lib) types;
+in {
+  options = {
+    nixcv = {
+      test = {
+        personal = lib.mkOption {
+          type = types.submodule {
+            options = (
+              lib.genAttrs
+              [
+                "basic"
+              ]
+              (
+                name:
+                  lib.mkOption {
+                    type = types.submoduleWith {
+                      modules = [../components/personal.nix];
+                    };
+                  }
+              )
+            );
+          };
+        };
+      };
+    };
+  };
+  config = {
+    nixcv = {
+      test = {
+        personal = {
+          basic = {
+            name = "John Doe";
+            label = "Programmer";
+            email = "john@doe.com";
+            phone = "(123) 456-7890";
+            url = "https://john.doe.com";
+            location = {
+              street = "123 Nunya Drive Unit 42";
+              city = "Clinton";
+              state = "MI";
+              country = "USA";
+              postalCode = "64735";
+            };
+            #profiles = {
+            #  profiles = [
+            #    {
+            #      url = "https://github.com/jdoe";
+            #    }
+            #    {
+            #      url = "https://mastodon.com/@jdoe";
+            #    }
+            #  ];
+            #};
+          };
+        };
+      };
+    };
+  };
+}
