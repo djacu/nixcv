c8a65ba786845f8771254bac20ee2b1c31c8e254
Daniel Baker <daniel.baker@tweag.io>
2024-01-15 21:05:44 -0800

    Add new tests for the skill and skills module.

---
+++ test/skills.nix
@@ -0,0 +1,24 @@
+{lib}: (
+  lib.mapAttrs
+  (
+    moduleName: moduleValue: (
+      lib.mapAttrs
+      (
+        testName: testValue: (
+          testValue._out
+        )
+      )
+      moduleValue
+    )
+  )
+  (
+    (lib.evalModules {
+      modules = [
+        ./modules/skills/skill.nix
+        ./modules/skills/skills.nix
+      ];
+    })
+    .config
+    .test
+  )
+)
