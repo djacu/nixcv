2b2003fd0a34c6174bea9770df86988c00cb33a7
Daniel Baker <daniel.n.baker@gmail.com>
2024-01-31 21:01:50 -0800

    Added a new test for the cv module that tests templating.

--- test/modules/top/cv.nix
+++ test/modules/top/cv.nix
@@ -10,6 +10,7 @@ in {
           [
             "basic"
             "bibliography"
+            "template"
           ]
         );
       };
@@ -446,6 +447,42 @@ in {
                   url = "https://nixcon.org/talks/how-to-use-modules";
                   keywords = "talk,nix,modules";
                 };
+              };
+            };
+          };
+
+        template =
+          basic
+          // {
+            template = {
+              blockquote = {
+                type = "newenvironment";
+                name = "blockquote";
+                begin = ''
+                  % https://tex.stackexchange.com/questions/97782/combining-leftskip-rightskip-and-raggedright
+                  \par%
+                  \medskip%
+                  \leftskip=2.5em%
+                  \advance\csname @rightskip\endcsname 2em \advance\rightskip 2em%
+                  %\rightskip=2em % switch above to this for justified and hyphenated right
+                  \noindent%
+                  \ignorespaces%
+                  %'';
+                end = ''
+                  \par%
+                  \medskip%
+                  \ignorespacesafterend%
+                  %'';
+              };
+              sectionFormat = {
+                type = "titleformat";
+                command = ''\section'';
+                shape = "hang";
+                format = ''\LARGE\bfseries'';
+                label = "";
+                sep = "0em";
+                before-code = ''\MakeUppercase'';
+                after-code = ''\color{black}\titlerule'';
               };
             };
           };
