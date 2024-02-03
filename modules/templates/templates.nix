294ae0af75bfa6f393bbe882e93d1e357f605e7c
Daniel Baker <daniel.n.baker@gmail.com>
2024-02-03 13:41:05 -0800

    Moved the content of the templates submodule to its own file so I can hide the suboptions on the cv page but show them all on their own docs page.

--- modules/templates/templates.nix
+++ modules/templates/templates.nix
@@ -11,79 +11,8 @@ in {
   options = {
     templates = lib.mkOption {
       description = "FIXME";
-      #visible = "shallow";
-      type = types.submodule {
-        imports = [
-          (
-            import
-            ../components/taggedName.nix
-            {
-              name = "environments";
-              submodules = [
-                ../templates/newenvironment.nix
-              ];
-              ordered = false;
-            }
-          )
-
-          (
-            import
-            ../components/taggedName.nix
-            {
-              name = "titlesec";
-              submodules = [
-                ../titlesec/titleformat.nix
-              ];
-              ordered = false;
-            }
-          )
-
-          (
-            import
-            ../components/taggedName.nix
-            {
-              name = "latex";
-              submodules = [
-                ../components/rawlatex.nix
-              ];
-              ordered = false;
-            }
-          )
-        ];
-
-        options = {
-          templateFile = lib.mkOption {
-            description = "A LaTeX template file. Will be used instead of any module settings.";
-            type = types.nullOr types.path;
-            default = null;
-          };
-
-          enumitem = lib.mkOption {
-            description = "Defines new lists and sets list options.";
-            type = modulesLib.pathsToTaggedSubmodules [
-              ../enumitem/enumitem.nix
-            ];
-            default = {
-              type = "enumitem";
-            };
-          };
-          layout = lib.mkOption {
-            description = "Defines the document class and basic paper/text styles.";
-            type = modulesLib.pathsToTaggedSubmodules [
-              ../templates/layout.nix
-            ];
-            default = {
-              type = "layout";
-              #documentclass = "article";
-              #papersize = "letterpaper";
-              #textwidth = "7in";
-              #textheight = "9in";
-              #textsize = "10pt";
-              #pagestyle = "empty";
-            };
-          };
-        };
-      };
+      visible = "shallow";
+      type = types.submodule ../templates/template.nix;
     };
 
     _out = {
