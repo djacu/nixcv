8023e37f4e031f610964d9d964338ca07bcbc9e5
Daniel Baker <daniel.n.baker@gmail.com>
2024-02-05 10:00:16 -0800

    Added the new template config to the tests.

--- test/modules/top/cv.nix
+++ test/modules/top/cv.nix
@@ -455,41 +455,7 @@ in {
         template =
           basic
           // {
-            templates = {
-              environments = {
-                blockquote = {
-                  type = "newenvironment";
-                  name = "blockquote";
-                  begin = ''
-                    % https://tex.stackexchange.com/questions/97782/combining-leftskip-rightskip-and-raggedright
-                    \par%
-                    \medskip%
-                    \leftskip=2.5em%
-                    \advance\csname @rightskip\endcsname 2em \advance\rightskip 2em%
-                    %\rightskip=2em % switch above to this for justified and hyphenated right
-                    \noindent%
-                    \ignorespaces%
-                    %'';
-                  end = ''
-                    \par%
-                    \medskip%
-                    \ignorespacesafterend%
-                    %'';
-                };
-              };
-              titlesec = {
-                sectionFormat = {
-                  type = "titleformat";
-                  command = ''\section'';
-                  shape = "hang";
-                  format = ''\LARGE\bfseries'';
-                  label = "";
-                  sep = "0em";
-                  before-code = ''\MakeUppercase'';
-                  after-code = ''\color{black}\titlerule'';
-                };
-              };
-            };
+            templates = import ../../../templates/simple.nix;
           };
       };
     };
