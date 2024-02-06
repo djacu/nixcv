a04716915585bc0b1afa8e850703cf85e3ea5758
Daniel Baker <daniel.n.baker@gmail.com>
2024-02-05 20:27:59 -0800

    Added a bibliography example.

---
+++ examples/sections/bibresouces.nix
@@ -0,0 +1,44 @@
+{
+  how-to-use-nix-talk = {
+    type = "online";
+    entryKey = "basic";
+    requiredFields = {
+      author = "John Doe";
+      title = "How to use Nix";
+      date = "2022-04";
+    };
+    optionalFields = {
+      language = "english";
+      organization = "NixCon";
+      url = "https://nixcon.org/talks/how-to-use-nix";
+      keywords = "talk,nix";
+    };
+  };
+  how-to-use-nix-paper = {
+    type = "online";
+    entryKey = "all-options";
+    requiredFields = {
+      author = "John Doe";
+      title = "How to use Nix";
+      date = "2022-05";
+    };
+    optionalFields = {
+      subtitle = "The most gentle intro";
+      titleaddon = "A story of reproducibility.";
+      language = "english";
+      editor = "Jane Doe";
+      version = "0.1.0";
+      note = "Nothing of note here!";
+      organization = "The NixOS Foundation";
+      url = "https://nixos.org/papers/how-to-use-nix";
+      urldate = "2023-01-15";
+      doi = "10.1234/example-doi";
+      eprint = "arXiv:1234.5678";
+      eprintclass = "cs.AI";
+      eprinttype = "preprint";
+      addendum = "Additional Information";
+      pubstate = "forthcoming";
+      keywords = "papers,nix";
+    };
+  };
+}
